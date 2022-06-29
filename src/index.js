7ce5e6437c70c56697e7ad987a09e28bf09b75b7
Vahin <danil.e.vakhrushev@gmail.com>
2022-06-29 06:10:55 +0500

    Добавил файлы стилей

--- src/index.js
+++ src/index.js
@@ -1,14 +1,1 @@
 import "./scss/style.scss";
-
-const userStack = {
-    language: "Javascript",
-    framework: "React",
-};
-
-const user = {
-    name: "Danil",
-    age: "30",
-    ...userStack,
-};
-
-console.log(user);
